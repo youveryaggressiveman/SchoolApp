c3055c0f3ee32febf2bfd2ce5ee9f9ac32fe59df
32-12 <32-12@mggtk.ru>
2022-03-01 15:57:05 +0300

    Добавьте файлы проекта.

---
+++ SchoolApp/Command/DelegateCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SchoolApp.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Func<object, bool> canExecute;
+        private readonly Action<object> execute;
+
+        public DelegateCommand(Func<object, bool> canExecute, Action<object> execute)
+        {
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+
+        public DelegateCommand(Action<object> execute) : this(null, execute) { }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute != null)
+            {
+                return canExecute(parameter);
+            }
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute?.Invoke(parameter);
+        }
+    }
+}
---
+++ SchoolApp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApp.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
